d437f48ae6def6058cd8391501047f1c7ed95478
ghozziayoub <ghozziayoub@gmail.com>
2021-03-17 20:10:27 +0100

    add role to user

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role:{
+        type:String,
+        default:"user"
     }
 
 })
